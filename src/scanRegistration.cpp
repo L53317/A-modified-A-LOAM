d69423cfb9f93c30f323b1f8edc5460cd855d86d
liu <liuxh.0@qq.com>
2023-08-01 21:32:01 +0200

    Replace the cloud point rostopic published by ouster lidar with /points.

--- src/scanRegistration.cpp
+++ src/scanRegistration.cpp
@@ -482,7 +482,8 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    ros::Subscriber subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>("/velodyne_points", 100, laserCloudHandler);
+    // ros::Subscriber subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>("/velodyne_points", 100, laserCloudHandler);
+    ros::Subscriber subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>("/points", 100, laserCloudHandler); // for ouster
 
     pubLaserCloud = nh.advertise<sensor_msgs::PointCloud2>("/velodyne_cloud_2", 100);
 
